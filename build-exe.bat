3d0333c02a3b7e1f2dcea6948b576297ea0e24f2
DELTAURI <deltauri@rekaciptagaram.co.id>
2026-09-05 01:20:54 +0700

    Menambahkan perintah taskkill otomatis sebelum proses kompilasi pada build-exe.bat

--- build-exe.bat
+++ build-exe.bat
@@ -3,7 +3,11 @@ title Build Executable (.EXE) - PT. Reka Cipta Garam
 echo ===================================================
 echo  PT. REKA CIPTA GARAM - SALT WEIGHING SYSTEM
 echo  Building Windows Executable (.EXE)...
-echo ===================================================
+echo Terminating any active application instances...
+taskkill /F /IM "RCG Salt Weighing System.exe" /T >nul 2>&1
+taskkill /F /IM "RCG Salt Weighing System 8.0.0.exe" /T >nul 2>&1
+taskkill /F /IM "electron.exe" /T >nul 2>&1
+
 call npm install
 call npm run dist
 echo.
